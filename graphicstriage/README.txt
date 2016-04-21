ebb73b7d3c700f9919c4ad1aff66eb343380da1b
Anthony Hughes <anthony.s.hughes@gmail.com>
2016-04-21 15:09:09 -0700

    Initial commit

---
+++ graphicstriage/README.txt
@@ -0,0 +1,2 @@
+This tracks progress on daily graphics bug and crash triage and is based on https://github.com/mozilla/ReleaseHealth.
+More information about this project can be found at https://wiki.mozilla.org/QA/Platform/Graphics/Triage.
---
+++ graphicstriage/js/triage.js
@@ -0,0 +1,138 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var BIG_SCREEN = "bigscreen";
+var SMALL_SCREEN = "smallscreen";
+
+var BUGZILLA_URL;
+var BUGZILLA_REST_URL;
+var bugQueries;
+
+$(document).ready(function () {
+  $.getJSON('js/triage.json', function(data) {
+    main(data);
+  });
+});
+
+function main(triage)
+{
+  BUGZILLA_URL = triage.BUGZILLA_URL;
+  BUGZILLA_REST_URL = triage.BUGZILLA_REST_URL;
+  var display = getDisplay();
+  bugQueries = triage.bugQueries;
+  var count = setupQueryURLs(triage.basequery);
+  displayTitle(count, "current");
+  displayQueries();
+  getBugCounts();
+}
+
+function getDisplay()
+{
+  var display = $.url().param('display');
+  if (display && (display === BIG_SCREEN)) {
+    return BIG_SCREEN;
+  }
+  return SMALL_SCREEN;
+}
+
+function displayTitle(count, displayType)
+{
+  //$("#title").append(" " + year);
+  $("#header-bg").attr("class", "header-bg header-bg-" + displayType);
+  if (displayType != "current") {
+    $("#title").attr("class", "title-light");
+    $("#subtitle").attr("class", "subtitle title-light");
+  }
+
+  var content = "";
+  if (bugQueries) {
+    for (var i = count-1; i>=0; i--) {
+      content += "<div class=\"bugcount\" id=\"reportDiv-" + i + "\"></div>\n";
+    }
+    $("#content").replaceWith(content);
+  }
+}
+
+function displayQueries()
+{
+  if (!bugQueries) {
+    return;
+  }
+  for (var i = 0; i < bugQueries.length; i++) {
+    var query = bugQueries[i];
+    if (!("url" in query)) {
+      continue;
+    }
+    console.log(query.name);
+    var day = new Date();    
+    day.setDate(day.getDate() + (query.day + (7-day.getDay())) % 7);
+    $("#reportDiv-" + i).replaceWith("<div class=\"bugcount\"><h3>"
+                                  + query.name
+                                  + "</h3>"
+                                  + "<div id=\"data" + i + "\""
+                                  + " class=\"data greyedout\">?</div>"
+                                  + "<br /><br /><b>Next Session:</b> <br />" + day.toDateString() + "<br />10:00am Pacific</div>");
+  }
+}
+
+function displayYearFooter(currentYear, displayType)
+{
+  var footer = "<div id=\"footer\" class=\"footer-" + displayType + "\">Year &gt; ";
+  for (var year=currentYear; year >= 2015; year --) {
+    footer += "<a href=\"?year=" + year + "\">" + year + "</a> | ";
+  }
+  footer += "<a href=\"?year=" + currentYear + "&future=1\">Scheduled</a>";
+  footer += "</div>";
+  $("#body").append(footer);
+}
+
+function setupQueryURLs(url)
+{
+  if (!bugQueries) {
+    return 0;
+  }
+  for (var i = 0; i < bugQueries.length; i++) {
+    bugQueries[i]["url"] = url + bugQueries[i]["params"];
+  }
+  return bugQueries.length;
+}
+
+function getBugCounts()
+{
+  if (!bugQueries) {
+    return;
+  }
+  for (var i = 0; i < bugQueries.length; i++) {
+    var bugQuery = bugQueries[i];
+    if (!("url" in bugQuery)) {
+      continue;
+    }
+    $.ajax({
+      url: BUGZILLA_REST_URL + bugQuery.url + '&count_only=1',
+      bugQuery: bugQuery,
+      index: i,
+      crossDomain:true,
+      dataType: 'json',
+      ifModified: true,
+      success: function(data, status) {
+        if (status === 'success') {
+          this.bugQuery.count = data.bug_count;
+          displayCount(this.index, this.bugQuery.count,
+                       BUGZILLA_URL + this.bugQuery.url);
+        }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert(textStatus);
+      }
+    });
+  }
+}
+
+function displayCount(index, count, url)
+{
+  $("#data" + index).replaceWith("<div class=\"data\"><a href=\"" + url
+                                 + "\">" + count + "</a></div>" );
+}
